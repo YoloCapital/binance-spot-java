9c37b895e4422126e27e808a7dd042702adb12f4
Andrew Kim <ewmkkpe@gmail.com>
2021-02-11 22:44:00 +0900

    add some comments

--- src/main/java/com/binance/api/client/domain/UniversalTransferType.java
+++ src/main/java/com/binance/api/client/domain/UniversalTransferType.java
@@ -4,10 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public enum UniversalTransferType {
-    MAIN_MARGIN,
-    MAIN_UMFUTURE,
-    MAIN_CMFUTURE,
-    MARGIN_MAIN,
-    UMFUTURE_MAIN,
-    CMFUTURE_MAIN,
+    MAIN_MARGIN,    // spot -> margin
+    MAIN_UMFUTURE,  // spot -> USDT-margined future
+    MAIN_CMFUTURE,  // spot -> coin-margined future
+    MARGIN_MAIN,    // margin -> spot
+    UMFUTURE_MAIN,  // USDT-margined future -> spot
+    CMFUTURE_MAIN,  // coin-margined future -> spot
 }
